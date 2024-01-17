3e7d16eb09e1c15c2740e68e03a92f55c7009017
FundaErmete <funda.ermete@gmail.com>
2024-01-17 16:55:35 +0100

    Create a card

--- PHP-CRUD/create.php
+++ PHP-CRUD/create.php
@@ -8,3 +8,15 @@ require_once 'Classes/CardRepository.php';
 
 $databaseManager = new DatabaseManager($config['host'], $config['user'], $config['password'], $config['dbname']);
 $cardRepository = new CardRepository($databaseManager);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $databaseManager->sanitize($_POST['name']);
+    $description = $databaseManager->sanitize($_POST['description']);
+
+    // Create a card
+    $cardRepository->create(['name' => $name, 'description' => $description]);
+
+    // Redirect to the overview page
+    header("Location: overview.php");
+    exit();
+}
